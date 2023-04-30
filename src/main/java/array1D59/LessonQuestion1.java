83d84734d2b6880dd425b27e44f331cde9d314d5
Bapun Parida <pariabapun@gmail.com>
2023-04-30 14:23:40 +0530

    Added

---
+++ src/main/java/array1D59/LessonQuestion1.java
@@ -0,0 +1,20 @@
+package array1D59;
+//Given an array find the max sum of contiguous elements of the array
+
+public class LessonQuestion1 {
+
+	// BF
+	public int sumArr(int arr[]) {
+
+		int n = arr.length;
+		for (int i = 0; i < n; i++) {
+
+			for (int j = i; j < n; j++) {
+				int sum =0;
+			}
+		}
+
+		return 1;
+
+	}
+}
